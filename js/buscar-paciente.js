c85c975d8c314973542d7feed51b34323741e73d
LucaPinheiro <lucapgomes11@gmail.com>
2023-06-25 23:46:09 -0300

    Configurando requisição e AJAX

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -1,15 +1,32 @@
 var botaoAdcionar = document.querySelector("#buscar-pacientes");
 
-botaoAdcionar.addEventListener("click", function() {
-    console.log("buscando pacientes");
-    
-    var xhr = new XMLHttpRequest();
+botaoAdcionar.addEventListener("click", function () {
+  console.log("buscando pacientes");
 
-    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
-    
-    xhr.addEventListener("load", function() {
-        console.log(xhr.responseText);
-    });
+  var xhr = new XMLHttpRequest();
 
-    xhr.send();
+  xhr.open(
+    "GET",
+    "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json"
+  );
+
+  xhr.addEventListener("load", function () {
+    var erroAjax = document.querySelector("#erro-ajax");
+
+    if (xhr.status == 200) {
+      erroAjax.classList.add("invisivel");
+      var resposta = xhr.responseText;
+      var pacientes = JSON.parse(resposta);
+
+      pacientes.forEach(function (paciente) {
+        adicionaPacienteNaTabela(paciente);
+      });
+    } else {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+      erroAjax.classList.remove("invisivel");
+    }
+  });
+
+  xhr.send();
 });
